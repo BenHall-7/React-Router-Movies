96953ee82084c89fd6e1344dfebe0002d8a61227
BenHall-7 <benjaminjahall@gmail.com>
2019-12-05 20:55:16 -0500

    Fix Route path and selected movie ID prop

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -5,20 +5,15 @@ const Movie = (props) => {
   const [movie, setMovie] = useState();
  
   useEffect(() => {
-    const id = 1;
-    // change ^^^ that line and grab the id from the URL
-    // You will NEED to add a dependency array to this effect hook
+    axios.get(`http://localhost:5000/api/movies/${props.match.params.id}`)
+      .then(response => {
+        setMovie(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
 
-       axios
-        .get(`http://localhost:5000/api/movies/${id}`)
-        .then(response => {
-          setMovie(response.data);
-        })
-        .catch(error => {
-          console.error(error);
-        });
-
-  },[]);
+  },[movie]);
   
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
